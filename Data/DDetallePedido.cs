296d42adef4265d91cf1825b81d7b43bfcfa4de7
Sebastián <sebas14121999@gmail.com>
2020-04-20 23:57:48 -0500

    Agregar archivos de proyecto.

---
+++ Data/DDetallePedido.cs
@@ -0,0 +1,48 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class DDetallePedido
+    {
+        public List<DetallePedido> GetDetallePedidos (DetallePedido detallePedido)
+        {
+            SqlParameter[] parameters = null;
+            string comandText = string.Empty;
+            List<DetallePedido> detallesPedidos = null;
+            try
+            {
+                comandText = "USP_ListarDetalles";
+                parameters = new SqlParameter[2];
+                parameters[0] = new SqlParameter("@IdPedido", SqlDbType.Int);
+                parameters[0].Value = detallePedido.IdProducto;
+                detallesPedidos = new List<DetallePedido>();
+
+                using (SqlDataReader reader = SQLHelper.ExecuteReader(SQLHelper.Connection, "USP_ListarDetalles", CommandType.StoredProcedure, parameters))
+                {
+                    while (reader.Read())
+                    {
+                        detallesPedidos.Add(new DetallePedido
+                        {
+                            IdProducto = reader["IdProducto"] != null ? Convert.ToInt32(reader["IdProducto"]) : 0,
+                            PrecioUnidad = reader["preciounidad"] != null ? Convert.ToInt32(reader["IdProducto"]) : 0,
+                            Cantidad = reader["cantidad"] != null ? Convert.ToInt32(reader["cantidad"]) : 0,
+                            Descuento = reader["descuento"] != null ? Convert.ToInt32(reader["descuento"]) : 0,
+                        });
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return detallesPedidos;
+        }
+    }
+}
---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Business;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConsultar_Click(object sender, RoutedEventArgs e)
+        {
+            BPedido bPedido = null;
+            try
+            {
+                bPedido = new BPedido();
+                dvgPedido.ItemsSource = bPedido.GetPedidosEntreFechas(Convert.ToDateTime(txtFechaInicio.Text), Convert.ToDateTime(txtFechaFin.Text));
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Comunicarse con el administrador");
+            }
+            finally
+            {
+                bPedido = null;
+            }
+        }
+
+        private void dvgPedido_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BDetallePedido bDetallePedido = null;
+            
+            try
+            {
+                //int idPedido = 10248;
+                //bDetallePedido = new BDetallePedido();
+                //dvgDetallePedido.ItemsSource = bDetallePedido.GetDetallePedidoPorId(idPedido);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Comunicarse con el administrador otra vez");
+            }
+            finally
+            {
+                bDetallePedido = null;
+            }
+        }
+    }
+}
